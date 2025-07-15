b54816de96a27887a5d31c819ee76df86551f11d
Marcelo <fernandesnunes026@gmail.com>
2025-07-15 01:17:20 -0400

    Uso do switch case, if e else, do while em uma construção de um jogo do acerto do numero secreto

---
+++ Atividade-1/Program.cs
@@ -0,0 +1,116 @@
+﻿
+
+Console.WriteLine(@"
+██████████████████████████████████████████████████████████████████████████████████████
+██▀▄─██─▄▄▄─█▄─▄▄─█▄─▄▄▀█─▄─▄─█▄─▄▄─███─▄▄─███▄─▀█▄─▄█▄─██─▄█▄─▀█▀─▄█▄─▄▄─█▄─▄▄▀█─▄▄─█
+██─▀─██─███▀██─▄█▀██─▄─▄███─████─▄█▀███─██─████─█▄▀─███─██─███─█▄█─███─▄█▀██─▄─▄█─██─█
+▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▀▄▄▀▀▄▄▄▀▀▄▄▄▄▄▀▀▀▄▄▄▄▀▀▀▄▄▄▀▀▄▄▀▀▄▄▄▄▀▀▄▄▄▀▄▄▄▀▄▄▄▄▄▀▄▄▀▄▄▀▄▄▄▄▀");
+void MenuJogo()
+{
+    while (true)
+    {
+        Console.WriteLine("Digite 1 para jogar");
+        Console.WriteLine("Digite -1 para sair...");
+
+        string opcaoEscolhida = Console.ReadLine();
+        int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+        if (!int.TryParse(opcaoEscolhida, out opcaoEscolhidaNumerica))
+        {
+            Console.WriteLine("Entrada inválida, digite um número.");
+            continue;
+        }
+        switch (opcaoEscolhidaNumerica)
+        {
+            case 1:
+                IniciarJogo();
+                break;
+            case -1:
+                Console.WriteLine("Você escolheu Sair! Tchau, Tchau :)");
+                return;
+            default:
+                Console.WriteLine("Opção inválida!");
+                break;
+        }
+        break;
+    }
+}
+MenuJogo();
+
+static void IniciarJogo()
+{
+    Random rand = new Random();
+    int numeroSecreto = rand.Next(1, 101);
+    int tentativa;
+
+    do
+    {
+        
+        Console.WriteLine("\n****************");
+        Console.Write("Faça seu palpite: ");
+        string entrada = Console.ReadLine();
+
+        if (!int.TryParse(entrada, out tentativa))
+        {
+            Console.WriteLine("Por favor, digite um número válido.");
+            continue;
+        }
+
+
+        if (tentativa < 1 || tentativa > 100)
+        {
+            Console.WriteLine("\nO número deve estar entre 1 e 100.");
+            Console.WriteLine($"Você digitou : {entrada}");
+
+            continue;
+        }
+
+        if (tentativa < numeroSecreto)
+        {
+            Console.WriteLine("\nSeu palpite é MENOR que o número secreto.");
+            Console.WriteLine($"Você digitou : {entrada}");
+
+        }
+        else if (tentativa > numeroSecreto)
+        {
+            Console.WriteLine("\nSeu palpite é MAIOR que o número secreto.");
+            Console.WriteLine($"Você digitou : {entrada}");
+
+        }
+        else
+        {
+            MostrarEfeitoAcerto();
+            break;
+
+        }
+    }
+    while (true);
+    Console.WriteLine("Fim de jogo, Obrigado Por jogar");
+}
+
+static void MostrarEfeitoAcerto()
+{
+    Console.Clear();
+    Console.ForegroundColor = ConsoleColor.Green;
+
+    string[] fogos = new string[]
+    {
+            "    *     *     *",
+            "  *   * *   * *   *",
+            "*  💥  💥  💥  💥  💥 *",
+            "  *   * *   * *   *",
+            "    *     *     *"
+    };
+
+    foreach (var linha in fogos)
+    {
+        Console.WriteLine(linha);
+        Thread.Sleep(300); // pausa para criar o efeito
+    }
+
+    Console.WriteLine("\n🎉 PARABÉNS! Você acertou o número secreto! 🎉");
+    Console.ResetColor();
+    Thread.Sleep(5500); // espera antes de voltar ao menu
+    Console.Clear();
+}
+
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Screnn Sound
+string mensagemDeBoasVindas = "Bem Vindo ao Screen Sound!";
+
+void MostrarMensagemDeBoasVindas()
+{
+    Console.WriteLine(@"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░");
+    Console.WriteLine(mensagemDeBoasVindas);
+}
+
+MostrarMensagemDeBoasVindas();
+
+void MostrarMenuDoPrograma()
+{
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir média de uma banda");
+    Console.WriteLine("Digite -1 para sair...");
+
+    Console.Write("\nDigite a sua opção: ");
+
+    string opcaoEscolhida = Console.ReadLine();
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    switch (opcaoEscolhidaNumerica)
+    {
+        case 1:
+            Console.WriteLine($"Você digitou : {opcaoEscolhidaNumerica}");
+            break;
+        case 2:
+            Console.WriteLine($"Você digitou : {opcaoEscolhidaNumerica}");
+            break;
+        case 3:
+            Console.WriteLine($"Você digitou : {opcaoEscolhidaNumerica}");
+            break;
+        case 4:
+            Console.WriteLine($"Você digitou : {opcaoEscolhidaNumerica}");
+            break;
+        case -1:
+            Console.WriteLine($"Tchau, Tchau :)");
+            break;
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+
+}
+MostrarMenuDoPrograma();
